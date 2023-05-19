0a13dddc5765637fbe575b6db76fb5a4f39b90f8
rafalradomski <rafal.radomski@hertz.com>
2023-05-19 16:51:29 +0100

    improve calcPoints unit tests

--- src/utils/calcPoints.test.js
+++ src/utils/calcPoints.test.js
@@ -2,15 +2,23 @@ import { calcPointsPerPurchase, calcTotalPoints } from './calcPoints';
 
 describe('calcPointsPerPurchase() should calculate points per purchase', () => {
   test('purchase above > 100', () => {
-    const res = calcPointsPerPurchase(120);
+    let res = calcPointsPerPurchase(120);
     expect(res).toEqual(90);
+    res = calcPointsPerPurchase(100);
+    expect(res).toEqual(50);
+    res = calcPointsPerPurchase(101);
+    expect(res).toEqual(52);
   });
   test('purchase in range: 50 - 100', () => {
-    const res = calcPointsPerPurchase(76);
+    let res = calcPointsPerPurchase(76);
     expect(res).toEqual(26);
+    res = calcPointsPerPurchase(50);
+    expect(res).toEqual(0);
+    res = calcPointsPerPurchase(51);
+    expect(res).toEqual(1);
   });
   test('purchase below 50', () => {
-    const res = calcPointsPerPurchase(46);
+    const res = calcPointsPerPurchase(49);
     expect(res).toEqual(0);
   });
 });
